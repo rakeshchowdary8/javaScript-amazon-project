bcefac598cc78ec66a96b32458a4f0196ec26b54
Rakesh Chowdary Vardhineni <rakeshchowdaryv7@gmail.com>
2026-02-07 10:27:24 +0530

    create order using the backend

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import {cart} from '../../data/cart.js';
 import { getProduct } from '../../data/products.js';
 import { getDeliveryOption } from '../../data/deliveryOptions.js';
 import { formatCurrency } from '../utils/money.js';
+import { addOrder } from '../../data/orders.js';
 
 
 export function renderPaymentSummary() {
@@ -27,7 +28,7 @@ export function renderPaymentSummary() {
           </div>
 
           <div class="payment-summary-row">
-            <div>Items (3):</div>
+            <div>Items (${cart.length}):</div>
             <div class="payment-summary-money">$${formatCurrency(totalBeforeTaxCents)}</div>
           </div>
 
@@ -51,9 +52,30 @@ export function renderPaymentSummary() {
             <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
           </div>
 
-          <button class="place-order-button button-primary">
+          <button class="place-order-button button-primary js-place-order">
             Place your order
           </button>
    `;
-   document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML; 
+   document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;
+
+   document.querySelector('.js-place-order').addEventListener('click', async () => {
+  try {
+      const response = await fetch('https://supersimplebackend.dev/orders', {
+      method: 'POST',
+      headers: {
+        'content-type' : 'application/json'
+      },
+      body: JSON.stringify({
+        cart: cart
+      })
+    });
+    const order = await response.json();
+    console.log(order);
+    addOrder(order);
+  } catch(error) {
+    console.log('Unexpected eror occured.Please try again later <_>');
+   }
+   window.location.href = 'orders.html';
+});
 }
+
